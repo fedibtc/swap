795cfbc8b3e3f77f83fa19b5539e6ebce5e10b2e
Conner Ow <conner@connerow.dev>
2024-07-24 16:10:34 -0500

    fix: wasm

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.experiments = {
+        ...config.experiments,
+        asyncWebAssembly: true,
+      };
+    }
+    return config;
+  },
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api": ["./node_modules/tiny-secp256k1/lib/*.wasm"]
+    }
+  }
+};
 
 export default nextConfig;
